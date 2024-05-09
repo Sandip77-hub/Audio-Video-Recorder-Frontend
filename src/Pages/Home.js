9134b8398a8a465ce05115a48a863d004da5b5ef
Sandip77-hub <sandippariyar2000@gmail.com>
2024-05-09 10:19:14 +0545

    contact changes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,34 +1,17 @@
-import React, { useState, useEffect } from 'react';
+// React Component
+import React, { useEffect, useRef } from 'react';
 import './Home.css';
 import lionImage from './images/lion.jpg';
 import Tiger from './images/Tiger.webp';
 import Rhino from './images/Rhino.jpg';
 
 const Home = () => {
-  const sentences = [
-    "In nature, wildlife symbolizes beauty, resilience, and diversity.",
-    "Protecting it is crucial due to habitat loss, climate change, and human encroachment.",
-    "Wildlife recognition systems, utilizing AI and data analytics, aid in identifying species and monitoring biodiversity.",
-    "They foster wonder and connection by revealing hidden wildlife behavior patterns.",
-    "Embracing wildlife recognition is essential for conservation efforts.",
-    "Together, let's safeguard biodiversity and create a brighter future."
-  ];
-
-  const [activeIndex, setActiveIndex] = useState(0);
-  const [isHidden, setIsHidden] = useState(false);
+  const textRef = useRef(null);
 
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      setActiveIndex((prevIndex) => (prevIndex + 1) % sentences.length);
-      setIsHidden(false); // Show the paragraph before transitioning to the next sentence
-    }, 500); // Interval in milliseconds (e.g., 5000ms = 5 seconds)
-
-    return () => clearInterval(intervalId); // Cleanup function to clear interval on component unmount
-  }, [sentences.length]);
-
-  const handleTransitionEnd = () => {
-    setIsHidden(true); // Hide the paragraph after the animation ends
-  };
+    const textElement = textRef.current;
+    textElement.classList.add('typewriter');
+  }, []);
 
   return (
     <div className='home'>
@@ -43,19 +26,18 @@ const Home = () => {
           <img className='Rhion-image' src={Rhino} alt='Rhino' />
         </div>
       </div>
-
-      <div className='manage_paragraph'>
-        <p
-          className={`sentence ${isHidden ? 'hidden' : ''}`}
-          onTransitionEnd={handleTransitionEnd}
-        >
-          {sentences[activeIndex]}
-        </p>
-      </div>
-
-      <h3 className='feature'>SAVE, PRESERVE, AND CONSERVE WILDLIFE.</h3>
+      <p className='manage_paragraph font-normal text-justify text-sm' ref={textRef}>
+        <span className='typewriter'>
+          In nature, wildlife symbolizes beauty, resilience, and diversity. Protecting it is crucial due to habitat
+          loss, climate change, and human encroachment. Wildlife recognition systems, utilizing AI and
+          data analytics, aid in identifying species and monitoring biodiversity. They foster wonder and
+          connection by revealing hidden wildlife behavior patterns. Embracing wildlife recognition is essential
+          for conservation efforts. Together, let's safeguard biodiversity and create a brighter future.
+        </span>
+      </p>
+      <h3 className='feature'>SAVE,PRESERVE AND CONSERVE WILDLIFE.</h3>
       <p className='purpose'>
-        WildTrack AI model makes it easy to collect and manage your observations.
+        WildTrack AI model make it easy to collect and manage your observation.
       </p>
     </div>
   );
